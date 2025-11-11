b07f92ff11abee4f09d40871ee52a5dce82e54bf
n-miller77 <146680703+n-miller77@users.noreply.github.com>
2025-11-11 13:51:19 -0500

    Update Kraken_betadiversity.R

--- Kraken_betadiversity.R
+++ Kraken_betadiversity.R
@@ -1,3 +1,13 @@
+beta_diversity.py -i /storage/home/hcoda1/9/nmiller304/shared_project/Chattahooche-samples-work/concat_samples/kraken_output/bracken_output2/empty_genus_files/*bracken.G.out --type bracken > /storage/home/hcoda1/9/nmiller304/shared_project/Chattahooche-samples-work/concat_samples/beta_diversity_S_kreport.bracken.G.tsv
+
+beta_diversity.py \
+  -i /storage/home/hcoda1/9/nmiller304/shared_project/Chattahooche-samples-work/concat_samples/kraken_output/bracken_output2/empty_genus_files/*bracken.G.out \
+  --type bracken \
+  --level G \
+  > /storage/home/hcoda1/9/nmiller304/shared_project/Chattahooche-samples-work/concat_samples/beta_diversity_G_bracken_no_wwtp.tsv
+
+
+
 # Load required libraries
 library(gplots)
 library("pheatmap")
@@ -7,13 +17,13 @@ library(ape)
 #################### Beta Diversity Analysis ##########################
 #######################################################################
 
-basic_sample_metadata <- read.csv("Z:/PROJECTS/GaTech/Chattahoochee/chattahoochee_project/assets/basic_sample_metadata.csv")
+basic_sample_metadata <- read.csv("/storage/home/hcoda1/9/nmiller304/shared_project/Chattahooche-samples-work/concat_samples/basic_sample_metadata_no_wwtp.csv")
 
 ########################################
 ########### Create Heatmap #############
 ########################################
 
-input_data <- read.table("Z:/PROJECTS/GaTech/Chattahoochee/data/kraken2/beta_diversity_G_kreport.bracken.G.fixed.tsv", quote="\"", comment.char="", check.names=FALSE)
+input_data <- read.table("/storage/home/hcoda1/9/nmiller304/shared_project/Chattahooche-samples-work/concat_samples/beta_diversity_no_wwtp_kreport.bracken.G.fixed.tsv", quote="\"", comment.char="", check.names=FALSE)
 
 # Replace missing values with a specified value (e.g., 0)
 input_data[is.na(input_data)] <- 1
@@ -27,8 +37,8 @@ rownames(metadata_df) <- basic_sample_metadata$sample
 # Specify colors
 ann_colors = list(
   subsite = c(REG = "gray", INF = "yellow4", EFF = "yellow1", UP = "coral1", DOWN = "coral4"),
-  site = c(Background = "white", DAM = "aliceblue", RL = "lightblue1", SC = "lightblue3", CC = "lightblue4", S1 = "mediumpurple1", S2 = "mediumpurple3", S3 = "mediumpurple4"),
-  batch = c("period_2" = "darkolivegreen1", "period_3" = "darkolivegreen3", "period_4" = "darkolivegreen")
+  site = c( DAM = "aliceblue", RL = "lightblue1", SC = "lightblue3", CC = "lightblue4", S1 = "mediumpurple1", S2 = "mediumpurple3", S3 = "mediumpurple4"),
+  batch = c("period_1" = "darkolivegreen1", "period_2" = "darkolivegreen3", "period_3" = "darkolivegreen")
 )
 
 # Plot heatmap
@@ -39,3 +49,5 @@ pheatmap(input_data,
          xlab = "Samples",  # Set the x-axis label
          ylab = "Samples"   # Set the y-axis label
 )
+
+
